b893b34b3f4bb00782794b1dfa98462fe937b718
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-09-30 21:51:54 +0300

    Add PerevalViewSet

--- fstr/pereval/views.py
+++ fstr/pereval/views.py
@@ -1,1 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Pereval
+from .serializers import PerevalSerializer
+
+
+class PerevalViewSet(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
