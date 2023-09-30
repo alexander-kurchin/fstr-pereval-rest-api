6eb5730605b92a9065a5634d67f4dac6590cfca1
Alexander Kurchin <80466494+alexander-kurchin@users.noreply.github.com>
2023-09-30 21:48:42 +0300

    Add PerevalSerializer

---
+++ fstr/pereval/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from .models import Pereval
+
+
+class PerevalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pereval
+        fields = ('beauty_title',
+                  'title',
+                  'other_titles',
+                  'connect',
+                  'added_at',
+                  'user',
+                  'coord_id',
+                  'level_winter',
+                  'level_spring',
+                  'level_summer',
+                  'level_autumn')
